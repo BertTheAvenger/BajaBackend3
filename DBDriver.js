b35631642b8ba241eb1395dfeec8543bd0a22a19
BertTheAvenger <>
2020-03-13 12:50:09 -0700

    DB Access changes

--- DBDriver.js
+++ DBDriver.js
@@ -4,15 +4,11 @@ const dbName = "baja";
 const runCollection = "runs";
 const metaCollection = "runmeta";
 
-class DBDriver extends MongoClient {
+class DBAccessor extends MongoClient {
     constructor(...args) {
         super(...args);
+        this.dbRuns = [];
     }
-
-    async getRun(runId) {
-
-    }
-
 
     async initialize(...args) {
         await super.connect(...args);
@@ -22,14 +18,19 @@ class DBDriver extends MongoClient {
         this.runs = await this.DB.collection(runCollection);
         this.meta = await this.DB.collection(metaCollection);
 
-        /*try {
-            await this.runs.drop();
-            await this.meta.drop();
-        } catch (e) {console.log("Collections already didn't exist.")}
-        */
-
         console.log("Got Collections");
 
+        //Create a linked DBRun instance for each run in the meta collection.
+        for(const metaEntry of await this.meta.find({}).toArray()) {
+            this.dbRuns.push(await new DbRun.Builder()
+                .metaCollection(this.meta)
+                .runCollection(this.runs)
+                .runId(metaEntry.run)
+                .build()
+            )
+        }
+        console.log(this.dbRuns);
+    /*
         //Update each "realtime" run to be static/permanent
         await this.meta.updateMany({realTime: true}, {$set: {realTime: false, completed: true}});
 
@@ -45,7 +46,7 @@ class DBDriver extends MongoClient {
             }
             console.log("Deleted incomplete");
         }
-
+*/
     }
 
 
@@ -63,34 +64,98 @@ class DBDriver extends MongoClient {
 
     }
 
-    async createRun(runInfo) {
-
-    }
-
-    async deleteRun(runId) {
-        await this.meta.deleteOne({run: runId});
-        await this.runs.removeMany({run: runId});
-    }
-
-    async uploadRun(){
-
-
-    }
-
-
 }
+
+
+class DBRunBuilder {
+    constructor() {
+        return this;
+    }
+
+    fromMeta(metaData){
+        this.metaData = metaData;
+        return this;
+    }
+
+    runId(id){
+        this.id = id;
+        return this;
+    }
+
+    metaCollection(meta){
+        this.meta = meta;
+        return this;
+    }
+
+    runCollection(runs){
+        this.runs = runs;
+        return this;
+    }
+
+    async build(){
+        //Data verification
+        let assert = (assertion, errMsg) => {
+            if(!assertion){
+                console.error(`Error creating DbRun: ${errMsg}`);
+            }
+        };
+
+        assert(this.meta, "MetaCollection is required for DbRun creation!");
+        assert(this.runs, "RunCollection is required for DbRun creation!");
+        assert(this.id, "runId is required for DbRun creation!");
+
+        return new DbRun()
+            .setMetaCollection(this.meta)
+            .setRunCollection(this.runs)
+            .setRunId(this.id)
+            .initialize();
+    }
+}
+
 
 class DbRun {
-    constructor(dbDriver, runId) {
+    constructor() {
+        return this;
+    }
+
+    setMetaCollection(metaCollection) {
+        this.metaCollection = metaCollection;
+        return this;
+    }
+
+    setRunCollection(runCollection) {
+        this.runCollection = runCollection;
+        return this;
+    }
+
+    setRunId(runId) {
         this.runId = runId;
+        return this;
     }
 
-    populate(runId) {
+    async initalize(){
+        let foundRun = await this.metaCollection.find({
+            run: this.runId
+        }).toArray();
+        if(foundRun.length > 0) {
 
+
+        } else { //Run with ID doesn't already exist, create one.
+            await this.metaCollection.insertOne({
+                run: this.runId,
+                size: -1,
+                fileName: "",
+                realTime: false,
+                completed: false,
+                timeIngested: -1
+            });
+        }
     }
 
+    static Builder = DBRunBuilder;
 
 
 }
 
-module.exports = DBDriver;
+
+module.exports = DBAccessor;
